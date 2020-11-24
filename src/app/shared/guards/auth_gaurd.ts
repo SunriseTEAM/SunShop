9a249e277c762c6c812250f36f691e9604daeefd
anhtuan01673233538@gmail.com <anhtuan01673233538@gmail.com>
2020-11-25 01:23:28 +0700

    xac thuc checkout

--- src/app/shared/guards/auth_gaurd.ts
+++ src/app/shared/guards/auth_gaurd.ts
@@ -5,7 +5,15 @@ import { AuthService } from "../services/auth.service";
 import { tap } from "rxjs/operators";
 
 @Injectable()
-export class AuthGuard  {
+export class AuthGuard  implements CanActivate{
   constructor(private router: Router, private authService: AuthService) {}
+
+  canActivate(): boolean {
+    if (!this.authService.isAuthenticated()) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+    return true;
+  }
 
 }
