5ea7c924deeb7856a9c986020efc2ad4e7597443
anhtuan01673233538@gmail.com <anhtuan01673233538@gmail.com>
2020-11-29 13:14:54 +0700

    toi uu hoa code

--- src/app/views/pages/product/cart-products/cart-products.component.ts
+++ src/app/views/pages/product/cart-products/cart-products.component.ts
@@ -14,10 +14,7 @@ import {Router} from "@angular/router";
 })
 export class CartProductsComponent implements OnInit {
   cartProducts: Product[];
-  categoryList: any;
   cartObj = [];
-  productsList: any;
-  cartTotalPrice: any;
   showDataNotFound = true;
 
   // Not Found Message
@@ -35,40 +32,23 @@ export class CartProductsComponent implements OnInit {
   ngOnInit() {
     console.log(this.cartService.getCartOBj());
     this.getCartDetailsByUser();
-    // below function will be triggerd from when removing and qty  is changing..
     this.cartService.cartServiceEvent.subscribe((data) => {
       this.cartObj = this.cartService.getCartOBj();
-      this.cartTotalPrice = this.cartService.cartTotalPrice;
     });
   }
 
   getCartDetailsByUser() {
     this.http
-      .postRequestWithToken('api/addtocart/getCartsByUserId', {
-        "userId":
-          this.http.getLoginDataByKey("user_id")
-      })
+      .postRequestWithToken('api/addtocart/getCartsByUserId', {"userId": this.http.getLoginDataByKey("user_id")})
       .subscribe(
         (data: any) => {
           this.cartObj = data;
-          this.cartTotalPrice = this.getTotalAmounOfTheCart();
         },
         (error) => {
           alert('Error while fetching the cart Details');
         }
       );
   }
-
-  getTotalAmounOfTheCart() {
-    const obj = this.cartObj;
-    let totalPrice = 0;
-    // tslint:disable-next-line:forin
-    for (const o in obj) {
-      totalPrice = totalPrice + parseFloat(obj[o].price);
-    }
-    return totalPrice.toFixed(2);
-  }
-
   removeCartById(cartObj) {
     if (confirm('Are you sure want to delete..?')) {
       const id = cartObj.id;
@@ -76,36 +56,6 @@ export class CartProductsComponent implements OnInit {
       // @ts-ignore
       this.cartService.removeCart(id,userId);
     }
-  }
-
-  removeCartProduct(product: Product) {
-    this.productService.removeLocalCartProduct(product);
-
-    // Recalling
-    this.getCartProduct();
-  }
-
-  getCartProduct() {
-    this.cartProducts = this.productService.getLocalCartProducts();
-  }
-
-  getProductsByCateogy(obj) {
-    const request = {
-      cat_id: obj.id,
-    };
-    this.http
-      .postRequestWithToken("api/product/getProductsByCategory", request)
-      .subscribe(
-        (data) => {
-          this.productsList = data;
-          if (this.productsList.length == 0) {
-            alert("No Product is found..");
-          }
-        },
-        (error) => {
-          alert("Error in login " + error);
-        }
-      );
   }
 
 }
